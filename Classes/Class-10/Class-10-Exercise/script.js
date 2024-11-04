ad50050f2ef050c713d5e51c9e3a26f520db777d
laisvigas <laisvigas@outlook.com>
2024-11-04 20:56:15 +0000

    class 10 exercise

---
+++ Classes/Class-10/Class-10-Exercise/script.js
@@ -0,0 +1,68 @@
+/*
+1. Considerando as variáveos x = 7 e y = 8, crie um código que mostre:
+A soma dos números.
+A diferença entre os números.
+A multiplicação dos números.
+A divisão dos números.
+Dica: Utilize os operadores +, -, * e /.
+*/
+
+let x = 10, y = 8;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+/*
+2. Dado o valor let x = 10, utilize operadores de atribuição para modificar e exibir o valor de x da seguinte forma:
+Adicione 5 a x.
+Subtraia 3 de x.
+Multiplique x por 2.
+Divida x por 4.
+Dica: Utilize operadores como +=, -=, *=, e /=.
+*/
+
+console.log("--");
+console.log(x += 5);
+console.log(x -= 3);
+console.log(x *= 2);
+console.log(x /= 4);
+
+/*
+3. Escreva um código que tenha duas strings e verifica:
+Se as duas strings são iguais (utilizando ==).
+Se as duas strings são diferentes (utilizando !=).
+*/
+
+console.log("--");
+let fruta = "uva", flor = "tulipa";
+
+console.log(fruta == flor);
+console.log(fruta != flor);
+
+/*
+4. Escreva um código que tenha dois numeros e verifica:
+Se o primeiro numero é maior que o segundo.
+Se o primeiro numero é menor ou igual ao segundo.
+*/
+
+console.log("--");
+console.log(x > y);
+console.log(x < y);
+
+/*
+5. Crie um código que tenha três valores booleanos e mostre:
+A conjunção de todos eles.
+A disjunção de todos eles.
+A negação da disjunção de todos eles.
+Utilize os operadores lógicos &&, || e !
+*/
+console.log("--");
+let valor1 = false, valor2 = false, valor3 = true;
+
+console.log(valor1 && valor2 && valor3);
+console.log(valor1 || valor2 || valor3);
+console.log(!(valor1 || valor2 || valor3));
+
+
